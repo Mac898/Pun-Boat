c2f17482449e1196ee19584ce0afed61f9c2cafc
Macauley Lim <xmachd@gmail.com>
2019-05-09 08:09:36 +1000

    update v1.0 - currently broken'

--- Pun Boat/diceHandler.py
+++ Pun Boat/diceHandler.py
@@ -29,14 +29,19 @@ def diceHandle(dice: str):
         "highest": 0,
         "total": 0,
         "median": 0,
-        "mode": 0
+        "mode": 0,
+        "limit": 0,
+        "rolls": 0
         }
-    for test in range(0,limit):
+    for test in range(1,limit+1):
         diceresults["amount"+str(test)] = 0
     for r in dice:
-        for test in range(0,limit):
+        for test in range(1,limit+1):
             if r == test:
                diceresults["amount"+str(r)] = diceresults["amount"+str(r)] + 1
+    for calc in range(1, limit+1):
+        if diceresults["amount"+str(calc)] == 0:
+            diceresults.pop("amount"+str(calc))
                     
     diceaverage = statistics.mean(dice)
     dicelowest = min(dice)
@@ -45,8 +50,9 @@ def diceHandle(dice: str):
     dicemedian = statistics.median(dice)
     dicemode = max(set(dice), key=dice.count)
    
-
-    diceresults["rolls"] = dice
+    diceresults["limit"] = limit
+    diceresults["rolls"] = rolls
+    diceresults["rollresults"] = dice
     diceresults["average"] = diceaverage
     diceresults["lowest"] = dicelowest
     diceresults["highest"] = dicehighest
